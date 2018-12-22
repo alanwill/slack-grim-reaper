0dd63e263061e0353ebc01103e36429b853aed57
Alan Williams <alanwill81@gmail.com>
2018-12-21 21:29:02 -0800

    Write finisher url to slack

--- sam/functions/slack-user-reporter-finisher/handler.py
+++ sam/functions/slack-user-reporter-finisher/handler.py
@@ -15,7 +15,7 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(currentdir, "./vendored"))
 
 # Modules downloaded into the vendored directory
-
+import requests
 
 # AWS X-Ray
 from aws_xray_sdk.core import xray_recorder
@@ -28,18 +28,22 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 # Initialize AWS services
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
 
 # Initialize variables
 s3_bucket = os.environ["S3_BUCKET_NAME"]
 table_userprocessing = dynamodb.Table(os.environ['USER_PROCESSING_TABLE'])
+slack_webhook = os.environ["SLACK_WEBHOOK"]
+s3_obj_key = "user_report/{:%Y-%m-%d-%H-%M-%S}.csv".format(datetime.datetime.now())
 
 
 def handler(event, context):
     log.debug("Received event {}".format(json.dumps(event)))
 
-    generate_list(event['guid'])
+    convert_to_csv(generate_list(event['guid']))
+    post_to_slack(generate_url(s3_obj_key))
+
     return
 
 
@@ -53,23 +57,65 @@ def generate_list(guid):
     for user in response['Items']:
         user_list.append([user['email'], user['department'], user['division']])
 
-    convert_to_csv(user_list)
+    return user_list
 
 
 def convert_to_csv(buffer):
     csv_file_path = os.path.join(tempfile.gettempdir(), 'slack_users.csv')
+
     with open(csv_file_path, 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for line in buffer:
             writer.writerow(line)
-        write_to_s3(csv_file_path)
+
+        generate_url(s3_obj_key)
+
+    with open(csv_file_path, 'rb') as csv_file:
+        s3.upload_fileobj(csv_file, s3_bucket, s3_obj_key)
+
     csv_file.close()
 
     return
 
 
-def write_to_s3(file_path):
+def generate_url(s3_key):
+    url = s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': s3_bucket,
+            'Key': s3_key
+        },
+        ExpiresIn=604800
+    )
 
-    s3.Bucket(s3_bucket).upload_file(file_path, "user_report/{:%Y-%m-%d-%H-%M-%S}.csv".format(datetime.datetime.now()))
+    return url
+
+
+def post_to_slack(url):
+
+    slack_message = {
+        "text": "This is your daily user management report.",
+        "attachments": [
+            {
+                "pretext": "The user report is here: " + str(url),
+                "text": "Should I?",
+                "callback_id": "deactivate",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "deactivate",
+                        "text": ":no_entry_sign: Deactivate",
+                        "type": "button",
+                        "value": "deactivate"
+                    },
+                ]
+            }
+        ]
+    }
+
+    # Send notification
+    response = requests.post(slack_webhook, data=json.dumps(slack_message))
+    print(response)
 
     return
